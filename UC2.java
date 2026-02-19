a39d274ee30a45a40c40ffd8ea090e3d83cab271
Shreyan Majumdar <smffpu2007@gmail.com>
2026-02-19 23:51:04 +0530

    added file to uc4 branch

--- UC2.java
+++ UC2.java
@@ -1,4 +1,4 @@
-public class OOPSBANNERAPP {
+public class UC2{
     public static void main(String[] args) {
         System.out.println(" *****    *****    ******    ***** ");
         System.out.println("**   **  **   **   **   **  **   **");
